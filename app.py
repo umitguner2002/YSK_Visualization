104b31f042a44951925e628ad63f77734da1cee0
='umitguner2002 <='umitguner2002@hotmail.com>
2023-05-26 18:09:40 +0300

    '26052023_1809'

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os.path
 import time
 
 from flask import Flask, render_template
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -22,16 +23,29 @@ def fetch():
     link = "https://acikveri.ysk.gov.tr/"
     browser.get(link)
 
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.visibility_of_element_located((By.ID, "myModalClose")))
+    wait = WebDriverWait(browser, 30)
+    wait.until(ec.visibility_of_element_located((By.ID, "myModalClose")))
     browser.find_element(By.XPATH, "//button[@aria-label='Close']").click() #Close modalPage
     browser.find_element(By.XPATH, "//a[@id='navbarDropdown']").click() #Click Choose Election
     browser.find_element(By.XPATH, "//a[@data-target='#collapse6']").click()  # Click Choose Election
     browser.find_element(By.XPATH, "//div[@aria-labelledby='heading6'][1]").click()
-    browser.find_element(By.XPATH, "//a[@href ='/secim-sonuc-istatistik/secim-sonuc")
-    #Mehmet Emin
 
-    time.sleep(5)
+    wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "nav-link")))
+    browser.find_element(By.XPATH, "//a[@href='/secim-sonuc-istatistik/secim-sonuc']").click()
+
+    try:
+        wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "country-svg")))
+        file_path = "C:\\Users\\umitg\\Downloads\\SecimSonucIl.json"
+        if not os.path.exists(file_path):
+            browser.find_element(By.XPATH, "//button[@class='btn btn-sm btn-outline-dark mr-2'][2]").click()
+            time.sleep(5)
+            print("Json File is downloaded...")
+        else:
+            print("Json File is already exist...")
+    except Exception as err:
+        print(err)
+    finally:
+        browser.close()
 
 @app.route("/")
 def main():
