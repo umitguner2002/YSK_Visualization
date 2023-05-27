c72d6161e0e5053b83f97ba945af2c0e03eb7f80
='umitguner2002 <='umitguner2002@hotmail.com>
2023-05-27 21:05:30 +0300

    '27052023_21:05'

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import time
 from flask import Flask, render_template
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as exp_cond
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import pandas as pd
 import io
 
@@ -24,27 +25,21 @@ def fetch():
     browser.get(link)
 
     wait = WebDriverWait(browser, 30)
-    wait.until(exp_cond.visibility_of_element_located((By.ID, "myModalClose")))
-    browser.find_element(By.XPATH, "//button[@aria-label='Close']").click() #Close modalPage
-    browser.find_element(By.XPATH, "//a[@id='navbarDropdown']").click() #Click Choose Election
-    browser.find_element(By.XPATH, "//a[@data-target='#collapse6']").click()  # Click Choose Election
-    browser.find_element(By.XPATH, "//div[@aria-labelledby='heading6'][1]").click()
 
-    wait.until(exp_cond.visibility_of_element_located((By.CLASS_NAME, "nav-link")))
-    browser.find_element(By.XPATH, "//a[@href='/secim-sonuc-istatistik/secim-sonuc']").click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Close']"))).click() #Close modalPage
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@id='navbarDropdown']"))).click() #Click Choose Election
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@data-target='#collapse6']"))).click()  #
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@aria-labelledby='heading6'][1]"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/secim-sonuc-istatistik/secim-sonuc']"))).click()
 
-    wait.until(exp_cond.element_to_be_clickable((By.XPATH, "//*[name()='svg' and @class='country-svg']//*[@class='city'][1]"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@class='country-svg']//*[@il_id='1']")))
+    browser.execute_script("document.querySelector('#map > svg > g > path:nth-child(1)')")
 
-    # wait.until(exp_cond.visibility_of_element_located((By.CLASS_NAME, "country-svg")))
-    # browser.find_element(By.XPATH, "//*[name()='svg' and @class='country-svg']//*[@class='city'][1]").get_attribute("path")
-
-    # browser.find_element(By.XPATH, "//*[@class='country-svg']//*[@il_id='1']")
-    # wait.until(exp_cond.visibility_of_element_located((By.CLASS_NAME, "country-svg")))
-    time.sleep(30)
+    time.sleep(10)
 
     # browser.close()
     # try:
-    #     wait.until(exp_cond.visibility_of_element_located((By.CLASS_NAME, "country-svg")))
+    #     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "country-svg")))
     #     file_path = "C:\\Users\\umitg\\Downloads\\SecimSonucIl.json"
     #     if not os.path.exists(file_path):
     #         browser.find_element(By.XPATH, "//button[@class='btn btn-sm btn-outline-dark mr-2'][2]").click()
